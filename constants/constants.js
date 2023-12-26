d1605fe448041359b2196f3a2a715223fa2983a5
Nirbhay45 <66776016+Nirbhay45@users.noreply.github.com>
2023-12-26 17:13:35 +0530

    Update constants.js

--- constants/constants.js
+++ constants/constants.js
@@ -1,6 +1,6 @@
 // ALL THE CONSTANTS ARE DEFINED HERE
 
 module.exports.PORT = 8000;
-module.exports.MONGODB_URL = "mongodb+srv://nirbhay:lHwjMnP1tJ43n6sl@cluster0.v9oagcn.mongodb.net/?retryWrites=true&w=majority";
-module.exports.SESSION_SECRET_KEY = 'secretcode;)';
+module.exports.MONGODB_URL = "USE YOUR OWN 😂";
+module.exports.SESSION_SECRET_KEY = 'secretcode🙈';
 module.exports.APP_NAME = "Placement Cell";
